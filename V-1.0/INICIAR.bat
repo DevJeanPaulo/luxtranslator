921a83eb689703dee844970889967e835c21d00d
DevJeanPaulo <jn.paulo2020@gmail.com>
2026-07-17 14:05:32 +0100

    v1.5 final - fix paywall logo autostart

--- V-1.0/INICIAR.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "LuxTranslator Server" cmd /k "node server.js"
-timeout /t 3 /nobreak > nul
-start "LuxTranslator Ngrok" cmd /k "ngrok http 3000"
-timeout /t 4 /nobreak > nul
-start chrome "http://127.0.0.1:4040"
-pause
