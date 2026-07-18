68ccaf7ffe815b2b8f38a578042ffbeef919b2c8
DevJeanPaulo <jn.paulo2020@gmail.com>
2026-07-19 00:28:52 +0100

    Novo icone LuxTranslator

--- App/sw.js
+++ App/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'luxtranslator-v1';
+const CACHE = 'luxtranslator-v2';
 const ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
